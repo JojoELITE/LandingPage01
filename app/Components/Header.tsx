dd8417795c86be714d1f5a317bdf0919cf0f92ea
JojoELITE <jojoelite1014@gmail.com>
2024-08-05 14:22:33 +0100

    Je viens finir le header

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
-export default function Header() {
-  return (
-    <div>Header</div>
-  )
+function Header() {
+    return (
+
+        <nav className='flex justify-between p-8 cursor-pointer bg-[#291D5D]'>
+
+            <div>
+                Logo
+            </div>
+
+            <div className='flex items-center gap-6 '>
+                <ul className='flex gap-6 text-sm'>
+                    <li className=''>Schedule</li>
+                    <li className=''>Speakers</li>
+                    <li className=''>Ticket</li>
+                    <li className=''>Contact</li>
+                </ul>
+
+                <button className="border border-white hover:bg-white hover:text-[#291D5D] text-white text-sm py-0.5 px-4 rounded-full">
+                    Contact
+                </button>
+
+            </div>
+
+        </nav>
+
+
+
+    )
 }
+
+export default Header
